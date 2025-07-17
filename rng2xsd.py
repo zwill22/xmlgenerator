828810578796426c5dea74d53b27b38b59ae357b
Zack Williams <73105122+zwill22@users.noreply.github.com>
2025-07-17 15:24:20 +0100

    RNG converter version A3

--- rng2xsd.py
+++ rng2xsd.py
@@ -116,6 +116,16 @@ class RelaxNGToXSDConverter:
             xsd_elem = etree.SubElement(xsd_parent, f"{{{self.xsd_ns}}}element")
             xsd_elem.set('name', name)
 
+            # Check element content
+            children = list(rng_elem)
+
+            # Check for simple type with data
+            if len(children) == 1 and self._get_local_name(children[0].tag) == 'data':
+                # Simple type element
+                data_type = self._extract_data_type(children[0])
+                xsd_elem.set('type', data_type)
+                return xsd_elem
+
             # Check if element has content
             has_attributes = any(self._get_local_name(child.tag) == 'attribute'
                                  for child in rng_elem)
@@ -133,29 +143,38 @@ class RelaxNGToXSDConverter:
                     # Mixed content
                     complex_type.set('mixed', 'true')
 
-                # Create sequence for child elements
-                if has_elements or len(rng_elem) > 1:
-                    sequence = etree.SubElement(complex_type, f"{{{self.xsd_ns}}}sequence")
-                    content_parent = sequence
-                else:
-                    content_parent = complex_type
+                # Handle simple content with attributes
+                if has_text and has_attributes and not has_elements:
+                    simple_content = etree.SubElement(complex_type, f"{{{self.xsd_ns}}}simpleContent")
+                    extension = etree.SubElement(simple_content, f"{{{self.xsd_ns}}}extension")
 
-                # Process children
-                for child in rng_elem:
-                    child_tag = self._get_local_name(child.tag)
-                    if child_tag == 'attribute':
-                        self._convert_attribute(child, complex_type)
-                    elif child_tag == 'text' and not has_elements:
-                        # Simple content with attributes
-                        simple_content = etree.SubElement(complex_type, f"{{{self.xsd_ns}}}simpleContent")
-                        extension = etree.SubElement(simple_content, f"{{{self.xsd_ns}}}extension")
-                        extension.set('base', 'xs:string')
-                        # Move attributes to extension
-                        for attr in complex_type.findall(f"{{{self.xsd_ns}}}attribute"):
-                            complex_type.remove(attr)
-                            extension.append(attr)
+                    # Find the data type
+                    data_elem = rng_elem.find(f'.//{{{self.rng_ns}}}data')
+                    if data_elem is not None:
+                        base_type = self._extract_data_type(data_elem)
                     else:
-                        self._convert_pattern(child, content_parent)
+                        base_type = 'xs:string'
+                    extension.set('base', base_type)
+
+                    # Add attributes to extension
+                    for child in rng_elem:
+                        if self._get_local_name(child.tag) == 'attribute':
+                            self._convert_attribute(child, extension)
+                else:
+                    # Create sequence for child elements
+                    if has_elements or len(rng_elem) > 1:
+                        sequence = etree.SubElement(complex_type, f"{{{self.xsd_ns}}}sequence")
+                        content_parent = sequence
+                    else:
+                        content_parent = complex_type
+
+                    # Process children
+                    for child in rng_elem:
+                        child_tag = self._get_local_name(child.tag)
+                        if child_tag == 'attribute':
+                            self._convert_attribute(child, complex_type)
+                        elif child_tag != 'text' or has_elements:
+                            self._convert_pattern(child, content_parent)
 
             elif has_text:
                 # Simple type
@@ -174,22 +193,192 @@ class RelaxNGToXSDConverter:
             xsd_attr = etree.SubElement(xsd_parent, f"{{{self.xsd_ns}}}attribute")
             xsd_attr.set('name', name)
 
-            # Determine type from content
-            data_elem = rng_elem.find(f'.//{{{self.rng_ns}}}data')
-            value_elem = rng_elem.find(f'.//{{{self.rng_ns}}}value')
+            # Look for data type information
+            for child in rng_elem:
+                child_tag = self._get_local_name(child.tag)
 
-            if data_elem is not None:
-                xsd_type = self._get_xsd_type(data_elem.get('type', 'string'))
-                xsd_attr.set('type', xsd_type)
-            elif value_elem is not None:
-                # Fixed value
-                xsd_attr.set('fixed', value_elem.text)
-                xsd_attr.set('type', 'xs:string')
+                if child_tag == 'data':
+                    xsd_type = self._extract_data_type(child)
+                    xsd_attr.set('type', xsd_type)
+
+                    # Handle patterns/restrictions
+                    param_elems = child.findall(f'.//{{{self.rng_ns}}}param')
+                    if param_elems:
+                        self._add_restrictions(xsd_attr, child, xsd_type)
+                    break
+
+                elif child_tag == 'value':
+                    # Fixed value
+                    xsd_attr.set('fixed', child.text or '')
+                    xsd_attr.set('type', 'xs:string')
+                    break
+
+                elif child_tag == 'choice':
+                    # Enumeration
+                    self._convert_attribute_choice(child, xsd_attr)
+                    break
+
+                elif child_tag == 'text':
+                    xsd_attr.set('type', 'xs:string')
+                    break
             else:
+                # Default to string if no type specified
                 xsd_attr.set('type', 'xs:string')
 
             return xsd_attr
 
+    def _extract_data_type(self, data_elem):
+        """Extract the data type from a RelaxNG data element"""
+        # Get the type attribute
+        rng_type = data_elem.get('type')
+
+        if not rng_type:
+            # Check for datatypeLibrary
+            datatype_lib = data_elem.get('datatypeLibrary')
+            if datatype_lib and 'XMLSchema' in datatype_lib:
+                # Sometimes the type is in a different format
+                return 'xs:string'
+            return 'xs:string'
+
+        # Handle different type formats
+        if rng_type.startswith('xsd:'):
+            # Already in XSD format
+            return rng_type.replace('xsd:', 'xs:')
+        elif ':' in rng_type:
+            # Has namespace prefix
+            prefix, local_type = rng_type.split(':', 1)
+            return f'xs:{local_type}'
+        else:
+            # Map to XSD type
+            return self._get_xsd_type(rng_type)
+
+    def _convert_attribute_choice(self, choice_elem, xsd_attr):
+        """Convert attribute choice to enumeration"""
+        # Create a simple type with restriction
+        simple_type = etree.SubElement(xsd_attr.getparent(), f"{{{self.xsd_ns}}}simpleType")
+        restriction = etree.SubElement(simple_type, f"{{{self.xsd_ns}}}restriction")
+        restriction.set('base', 'xs:string')
+
+        # Add enumeration values
+        for value_elem in choice_elem.findall(f'.//{{{self.rng_ns}}}value'):
+            enum = etree.SubElement(restriction, f"{{{self.xsd_ns}}}enumeration")
+            enum.set('value', value_elem.text or '')
+
+        # Remove type attribute and link to simple type
+        if 'type' in xsd_attr.attrib:
+            del xsd_attr.attrib['type']
+
+        # Generate unique name for the simple type
+        type_name = f"{xsd_attr.get('name')}Type"
+        simple_type.set('name', type_name)
+        xsd_attr.set('type', type_name)
+
+        # Move simple type before the attribute
+        parent = xsd_attr.getparent()
+        parent.remove(simple_type)
+        parent.insert(parent.index(xsd_attr), simple_type)
+
+    def _add_restrictions(self, xsd_elem, data_elem, base_type):
+        """Add restrictions from RelaxNG param elements"""
+        params = {}
+        for param in data_elem.findall(f'.//{{{self.rng_ns}}}param'):
+            name = param.get('name')
+            if name:
+                params[name] = param.text
+
+        if params:
+            # Create inline simple type with restrictions
+            simple_type = etree.SubElement(xsd_elem.getparent(), f"{{{self.xsd_ns}}}simpleType")
+            restriction = etree.SubElement(simple_type, f"{{{self.xsd_ns}}}restriction")
+            restriction.set('base', base_type)
+
+            # Map RelaxNG params to XSD facets
+            param_mapping = {
+                'minLength': 'minLength',
+                'maxLength': 'maxLength',
+                'pattern': 'pattern',
+                'minInclusive': 'minInclusive',
+                'maxInclusive': 'maxInclusive',
+                'minExclusive': 'minExclusive',
+                'maxExclusive': 'maxExclusive',
+                'totalDigits': 'totalDigits',
+                'fractionDigits': 'fractionDigits'
+            }
+
+            for param_name, param_value in params.items():
+                if param_name in param_mapping:
+                    facet = etree.SubElement(restriction, f"{{{self.xsd_ns}}}{param_mapping[param_name]}")
+                    facet.set('value', param_value)
+
+            # Remove type attribute and use inline simple type
+            if 'type' in xsd_elem.attrib:
+                del xsd_elem.attrib['type']
+
+    def _get_xsd_type(self, rng_type):
+        """Map RelaxNG data types to XSD types"""
+        type_mapping = {
+            # String types
+            'string': 'xs:string',
+            'token': 'xs:token',
+            'normalizedString': 'xs:normalizedString',
+            'language': 'xs:language',
+            'Name': 'xs:Name',
+            'NCName': 'xs:NCName',
+            'NMTOKEN': 'xs:NMTOKEN',
+            'NMTOKENS': 'xs:NMTOKENS',
+
+            # Numeric types
+            'integer': 'xs:integer',
+            'positiveInteger': 'xs:positiveInteger',
+            'negativeInteger': 'xs:negativeInteger',
+            'nonNegativeInteger': 'xs:nonNegativeInteger',
+            'nonPositiveInteger': 'xs:nonPositiveInteger',
+            'int': 'xs:int',
+            'unsignedInt': 'xs:unsignedInt',
+            'long': 'xs:long',
+            'unsignedLong': 'xs:unsignedLong',
+            'short': 'xs:short',
+            'unsignedShort': 'xs:unsignedShort',
+            'byte': 'xs:byte',
+            'unsignedByte': 'xs:unsignedByte',
+            'decimal': 'xs:decimal',
+            'float': 'xs:float',
+            'double': 'xs:double',
+
+            # Boolean
+            'boolean': 'xs:boolean',
+
+            # Date/Time types
+            'date': 'xs:date',
+            'dateTime': 'xs:dateTime',
+            'time': 'xs:time',
+            'duration': 'xs:duration',
+            'gYear': 'xs:gYear',
+            'gYearMonth': 'xs:gYearMonth',
+            'gMonth': 'xs:gMonth',
+            'gMonthDay': 'xs:gMonthDay',
+            'gDay': 'xs:gDay',
+
+            # Other types
+            'anyURI': 'xs:anyURI',
+            'base64Binary': 'xs:base64Binary',
+            'hexBinary': 'xs:hexBinary',
+            'QName': 'xs:QName',
+            'ID': 'xs:ID',
+            'IDREF': 'xs:IDREF',
+            'IDREFS': 'xs:IDREFS',
+            'ENTITY': 'xs:ENTITY',
+            'ENTITIES': 'xs:ENTITIES',
+        }
+        return type_mapping.get(rng_type, 'xs:string')
+
+    def _get_local_name(self, tag):
+        """Get local name from tag"""
+        if '}' in tag:
+            return tag.split('}')[1]
+        return tag
+
+    # ... (rest of the methods remain the same)
     def _convert_ref(self, rng_elem, xsd_parent):
         """Convert RelaxNG ref to corresponding pattern"""
         ref_name = rng_elem.get('name')
@@ -253,89 +442,10 @@ class RelaxNGToXSDConverter:
         # This is typically used for fixed values
         pass
 
-    def _get_xsd_type(self, rng_type):
-        """Map RelaxNG data types to XSD types"""
-        type_mapping = {
-            'string': 'xs:string',
-            'token': 'xs:token',
-            'integer': 'xs:integer',
-            'int': 'xs:int',
-            'long': 'xs:long',
-            'short': 'xs:short',
-            'byte': 'xs:byte',
-            'decimal': 'xs:decimal',
-            'float': 'xs:float',
-            'double': 'xs:double',
-            'boolean': 'xs:boolean',
-            'date': 'xs:date',
-            'dateTime': 'xs:dateTime',
-            'time': 'xs:time',
-            'duration': 'xs:duration',
-            'anyURI': 'xs:anyURI',
-            'ID': 'xs:ID',
-            'IDREF': 'xs:IDREF',
-            'NMTOKEN': 'xs:NMTOKEN',
-        }
-        return type_mapping.get(rng_type, 'xs:string')
-
-    def _get_local_name(self, tag):
-        """Get local name from tag"""
-        if '}' in tag:
-            return tag.split('}')[1]
-        return tag
-
-
-# Example usage
-def convert_relaxng_to_xsd(relaxng_path, xsd_path=None):
-    """
-    Convert a RelaxNG schema file to XSD
-
-    Args:
-        relaxng_path: Path to the RelaxNG file
-        xsd_path: Optional path for the output XSD file
-
-    Returns:
-        XSD content as string
-    """
-    converter = RelaxNGToXSDConverter()
-    return converter.convert(relaxng_path, xsd_path)
-
-
-# Integration function for your existing script
-def ensure_xsd_schema(schema_path):
-    """
-    Ensure the schema is in XSD format, converting from RelaxNG if necessary
-
-    Args:
-        schema_path: Path to the schema file
-
-    Returns:
-        Path to XSD file (either original or converted)
-    """
-    import tempfile
-    from pathlib import Path
-
-    schema_path = Path(schema_path)
-
-    if schema_path.suffix.lower() == '.xsd':
-        return str(schema_path)
-
-    elif schema_path.suffix.lower() in ['.rng', '.rnc']:
-        # Convert RelaxNG to XSD
-        converter = RelaxNGToXSDConverter()
-
-        # Create temporary XSD file
-        with tempfile.NamedTemporaryFile(mode='w', suffix='.xsd', delete=False) as tmp:
-            xsd_content = converter.convert(str(schema_path))
-            tmp.write(xsd_content)
-            return tmp.name
-
-    else:
-        raise ValueError(f"Unsupported schema format: {schema_path.suffix}")
-
 
 if __name__ == '__main__':
     import os
+
     converter = RelaxNGToXSDConverter()
     rng_dir = os.path.join("examples", "rng")
     for file in os.listdir(rng_dir):
