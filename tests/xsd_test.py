ba8a1bd32bb15d79660f1c7dc71cc3a677582490
Zack Williams <73105122+zwill22@users.noreply.github.com>
2025-07-18 13:19:34 +0100

    Added output validation to test

--- tests/xsd_test.py
+++ tests/xsd_test.py
@@ -1,12 +1,12 @@
 import pytest
+import xmlschema
 
 from pathlib import Path
 from typing import List
 from xmlgenerator import generate_xml_from_xsd
+from xmlgenerator.xsd2xml import CircularReferenceError
 
 __root_dir__ = Path(__file__).parent.parent
-
-from xmlgenerator.xsd2xml import CircularReferenceError
 
 
 def get_xsd_files() -> List[Path]:
@@ -14,16 +14,27 @@ def get_xsd_files() -> List[Path]:
     output = [path for path in examples_path.iterdir()]
     return output
 
+
 def test_empty_path():
     get_xsd_files()
     with pytest.raises(FileNotFoundError):
         generate_xml_from_xsd("")
 
-def test_xsd():
-    xsd_files = get_xsd_files()
-    for xsd_file in xsd_files:
-        try:
-            xml = generate_xml_from_xsd(str(xsd_file))
-            assert xml is not None
-        except CircularReferenceError as e:
-            assert e is not None
+
+def validate(xml: str, xsd_file: Path | str) -> None:
+    schema = xmlschema.XMLSchema(xsd_file)
+
+    try:
+        schema.validate(xml)
+    except xmlschema.XMLSchemaException as e:
+        pytest.fail(e)
+
+
+@pytest.mark.parametrize("xsd_file", get_xsd_files())
+def test_xsd(xsd_file):
+    try:
+        xml = generate_xml_from_xsd(str(xsd_file))
+        assert xml is not None
+        validate(xml, xsd_file)
+    except CircularReferenceError as e:
+        assert e is not None
