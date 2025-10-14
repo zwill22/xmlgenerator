e2422578f8db862ce0c6b9d6c319ffe438f94e94
Zack Williams <73105122+zwill22@users.noreply.github.com>
2025-10-14 14:26:30 +0100

    Add python wrapper for `xsd_test_data`

--- py_xml_generator/tests/test_xsd_repo.py
+++ py_xml_generator/tests/test_xsd_repo.py
@@ -1,48 +1,39 @@
-import pytest
-from pathlib import Path
 from typing import List
 
+import pytest
+from pathlib import Path
+
 import pyxmlgenerator
+from pyxsdtestdata import get_xsd_test_data
 
 from .common import validate_output, get_project_root
 
 
-def list_from_file(xsd_list: Path, xsd_dir: Path) -> List[Path]:
-    with open(xsd_list, 'r') as f:
-        files = f.readlines()
-
-    return [xsd_dir / file for file in files]
-
-
-def list_from_dir(xsd_dir: Path) -> List[Path]:
-    files = xsd_dir.rglob("*.xsd")
-    output = []
-    for xsd_file in files:
-        output.append(xsd_file)
-    return output
-
-
-def get_file_list(root_dir: Path, xsd_dir: Path) -> List[Path]:
-    xsd_list = root_dir / "xsd_list.txt"
-    if xsd_list.is_file():
-        return list_from_file(xsd_list, xsd_dir)
-    else:
-        return list_from_dir(xsd_dir)
-
-
-def fetch_xsd_files() -> List[Path]:
+def fetch_xsd_files(
+        xsd_directory: str = "xsdtests-master",
+        archive_filename: str = "xsdtests.zip",
+        include_extras: bool = True
+) -> List[Path]:
     root_dir = get_project_root()
-    xsd_dir = root_dir / "xsdtests-master"
+    xsd_dir_path = root_dir / xsd_directory
+    archive_path = root_dir / archive_filename
 
-    assert xsd_dir.is_dir()
+    data = get_xsd_test_data(xsd_dir_path, archive_path, include_extras)
 
-    files = get_file_list(root_dir, xsd_dir)
+    valid_files = []
+    for entry in data:
+        assert type(entry) is tuple
+        assert len(entry) == 2
 
-    return files
+        path: Path = entry[0]
+        valid: bool = entry[1]
 
+        if valid:
+            valid_files.append(path)
 
-data = fetch_xsd_files()
+    return valid_files
 
+valid_data = fetch_xsd_files()
 
 def id_fn(file: Path):
     return str(file)
@@ -53,7 +44,7 @@ def validate_schema(xml_generator, xsd_file: Path):
     xml_generator.validate(filepath)
 
 
-@pytest.mark.parametrize("file", data, ids=id_fn)
+@pytest.mark.parametrize("file", valid_data, ids=id_fn)
 def test_xsd_file(xml_generator, file):
     try:
         validate_schema(xml_generator, file)
---
+++ py_xsd_test_data/src/lib.rs
@@ -0,0 +1,45 @@
+use futures::executor::block_on;
+use pyo3::exceptions::{PyRuntimeError};
+use pyo3::prelude::*;
+use std::any::Any;
+use std::collections::HashSet;
+use std::panic;
+use std::path::PathBuf;
+use xsdtestdata::get_test_data;
+
+fn handle_panic(error: Box<dyn Any>) -> PyErr {
+    if let Some(s) = error.downcast_ref::<&str>() {
+        let msg = format!("{}", s);
+        PyRuntimeError::new_err(msg)
+    } else if let Some(s) = error.downcast_ref::<String>() {
+        let msg = format!("{}", s);
+        PyRuntimeError::new_err(msg)
+    } else {
+        PyRuntimeError::new_err("XMLGenerator: unknown error")
+    }
+}
+
+#[pyfunction]
+fn get_xsd_test_data(
+    database_dir: PathBuf,
+    archive_path: PathBuf,
+    include_extra_files: bool,
+) -> PyResult<HashSet<(PathBuf, bool)>> {
+    match panic::catch_unwind(|| {
+        block_on(get_test_data(
+            &database_dir,
+            &archive_path,
+            include_extra_files,
+        ))
+    }) {
+        Ok(result) => Ok(result),
+        Err(e) => Err(handle_panic(e)),
+    }
+}
+
+#[pymodule]
+fn pyxsdtestdata(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(get_xsd_test_data, m)?)?;
+
+    Ok(())
+}
