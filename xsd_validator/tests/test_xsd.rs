e27ff620bab46c713cbf6b362f4dc9abd87bd543
zwill22 <73105122+zwill22@users.noreply.github.com>
2026-03-17 09:22:45 +0000

    Added rstest to test_xsd

--- xsd_validator/tests/test_xsd.rs
+++ xsd_validator/tests/test_xsd.rs
@@ -1,35 +1,23 @@
 #[cfg(test)]
 mod tests {
-    use std::fs;
-    use std::fs::ReadDir;
-    use workspace_root::get_workspace_root;
+    use std::path::PathBuf;
     use xsdvalidator::XSDValidator;
+    use rstest::*;
 
-    fn fetch_test_files(directory: &str) -> ReadDir {
-        let root = get_workspace_root();
-
-        let example_dir = root.join("examples").join(directory);
-        let paths = fs::read_dir(example_dir).unwrap();
-
-        paths
+    #[fixture]
+    #[once]
+    fn validator() -> XSDValidator {
+        XSDValidator::new(true)
     }
 
-    fn validate_files(validator: &XSDValidator, files: ReadDir) {
-        for file in files {
-            let file = file.unwrap().path();
-            let result = validator.validate(&file);
-
-            assert!(result.is_ok());
-            assert!(result.unwrap());
-        }
-    }
-
-    #[test]
-    fn test_xsd() {
-        let validator = XSDValidator::new(true);
-
-        let files = fetch_test_files("working");
-
-        validate_files(&validator, files);
+    #[rstest]
+    fn test_xsd(
+        validator: &XSDValidator,
+        #[base_dir = ".."]
+        #[files("examples/working/*.xsd")]
+        path: PathBuf,
+    ) {
+       validator.validate(&path).expect("Validation failed");
     }
 }
+
