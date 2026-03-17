900a7c03e5c3a1b29a53cff86c6c3b61f09dd129
zwill22 <73105122+zwill22@users.noreply.github.com>
2026-03-17 16:04:10 +0000

    Removed rs test from test_xsd

---
+++ xsd_validator/tests/test_validate_examples.rs
@@ -0,0 +1,32 @@
+#[cfg(test)]
+mod tests {
+    use std::fs;
+    use std::fs::ReadDir;
+    use workspace_root::get_workspace_root;
+    use xsdvalidator::XSDValidator;
+
+    fn fetch_test_files(directory: &str) -> ReadDir {
+        let root = get_workspace_root();
+
+        let example_dir = root.join("examples").join(directory);
+        let paths = fs::read_dir(example_dir).expect("Unable to read examples directory");
+
+        paths
+    }
+
+    fn validate_files(validator: &XSDValidator, files: ReadDir) {
+        files.for_each(|file| {
+            let file = file.expect("File not found").path();
+            validator.validate(&file).expect("File is invalid");
+        });
+    }
+
+    #[test]
+    fn test_xsd() {
+        let validator = XSDValidator::new(true);
+
+        let files = fetch_test_files("working");
+
+        validate_files(&validator, files);
+    }
+}
