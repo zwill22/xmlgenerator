2af67b5dfd8323ae84b820bae1cdfa5bfb574670
zwill22 <73105122+zwill22@users.noreply.github.com>
2026-03-19 11:25:36 +0000

    Added checks for non-characters and private use

--- xml_generator/src/encoder.rs
+++ xml_generator/src/encoder.rs
@@ -1,6 +1,8 @@
 use crate::XMLGeneratorError;
 use crate::whitespace::WhiteSpace;
 use html_escape::encode_text;
+use unic_ucd::CharAge;
+use unic_char_basics::{is_noncharacter, is_private_use};
 
 pub(crate) fn decode_html(input: &str) -> String {
     html_escape::decode_html_entities(input).to_string()
@@ -18,6 +20,14 @@ fn filter(input: &str, whitespace: &WhiteSpace) -> Result<String, XMLGeneratorEr
         } else if c.is_control() {
             return Err(XMLGeneratorError::UnimplementedFeature(
                 "Control characters".to_string(),
+            ));
+        } else if is_noncharacter(c){
+            return Err(XMLGeneratorError::UnimplementedFeature("Non characters".to_string()));
+        } else if is_private_use(c) {
+            return Err(XMLGeneratorError::UnimplementedFeature("Private use characters".to_string()));
+        } else if c.age().is_none() {
+            return Err(XMLGeneratorError::UnimplementedFeature(
+                "Unassigned Unicode characters".to_string(),
             ));
         } else {
             out.push(c);
