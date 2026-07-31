2b8d7c745f49c75b540670a231d9fe87ac119d79
zwill22 <73105122+zwill22@users.noreply.github.com>
2026-07-31 15:03:26 +0100

    Added docstrings to xsdvalidator crate

--- xsd_validator/src/error.rs
+++ xsd_validator/src/error.rs
@@ -1,9 +1,17 @@
+/// Error handler for the XSD validator [crate].
+/// 
 #[derive(Debug)]
 pub enum XSDValidationError {
+    /// Invalid input path
     PathError,
+    /// Error occurred while interfacing with libxml2
     LibXML2InterfaceError(String),
+    /// Error redirecting libxml2 warnings
     OutputRedirectError(String),
+    /// Error parsing XSD file
     ParseError(String),
+    /// Error reading file with [file_to_string]
     ReadFileError,
+    /// Recursion encountered in XSD
     XSDRecursionError,
 }
--- xsd_validator/src/tracker.rs
+++ xsd_validator/src/tracker.rs
@@ -2,6 +2,33 @@ use crate::XSDValidationError;
 use roxmltree::Node;
 use std::collections::HashSet;
 
+/// Tracker struct used to track recursion in an [roxmltree] XSD/XML tree.
+///
+/// This struct holds unique values for all nodes previously visited by the parser.
+/// If a node is encountered a second time in the same branch, then an error is thrown.
+/// This is because a repeated cycle would cause infinite recursion in the tree.
+///
+/// [roxmltree]: https://crates.io/crates/roxmltree
+/// 
+/// # Fields
+///
+/// - `values` (`HashSet<u32>`) - Set of unique values for all parent nodes
+///
+/// # Example
+///
+/// ```rust
+/// use xsdvalidator::tracker::Tracker;
+/// use roxmltree::Document;
+///
+/// fn check_for_recursion(document: &Document) {
+///     let mut tracker = Tracker::new();
+///     let root = document.root();
+///
+///     if let Err(_) = tracker.check_node(&root) {
+///         panic!("Infinite recursion detected!");
+///     }
+/// }
+/// ```
 pub struct Tracker {
     values: HashSet<u32>,
 }
@@ -15,6 +42,12 @@ impl Default for Tracker {
 }
 
 impl Tracker {
+    /// Initialises an empty tracker with no nodes recorded
+    ///
+    /// # Returns
+    ///
+    /// - [Tracker] - An empty tracker object
+    ///
     pub fn new() -> Self {
         Default::default()
     }
@@ -43,6 +76,43 @@ impl Tracker {
         }
     }
 
+    /// Recursively check a node and all its children for recursion
+    ///
+    /// This method adds a nodes unique ID to the tracker and recursively calls the method on all child nodes.
+    /// If a node is added to the tracker that is already present, the method returns an [XSDValidationError::XSDRecursionError].
+    /// Once all of a node's children have been checked, the current node is removed from the tracker and the function exits.
+    ///
+    /// # Arguments
+    ///
+    /// - `node` (`&Node`) - The root [roxmltree] node to check
+    ///
+    /// # Returns
+    ///
+    /// - `Result<(), XSDValidationError>` - Returns ok if no recursion detected, else [XSDValidationError::XSDRecursionError]
+    ///
+    /// # Errors
+    ///
+    /// - [XSDValidationError] - An [XSDValidationError::XSDRecursionError] is raised if recursion is detected
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use xsdvalidator::tracker::Tracker;
+    /// use roxmltree::Node;
+    ///
+    /// fn check_node(tracker: &Tracker, node: &Node) {
+    ///     let result =  tracker.check_node(&root);
+    ///
+    ///     match result {
+    ///         Ok(_) => {
+    ///             println!("Node checked!")
+    ///         },
+    ///         Err(_) => {
+    ///             eprintln!("Infinite recursion detected!");
+    ///     }
+    /// }
+    /// ```
+    ///
     pub fn check_node(&mut self, node: &Node) -> Result<(), XSDValidationError> {
         self.add(node)?;
 
